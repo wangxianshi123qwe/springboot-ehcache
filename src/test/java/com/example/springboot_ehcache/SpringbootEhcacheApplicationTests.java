8b991c4bb85dbadcfcdb355ae9c40d40b33b7cd8
wxs <2513753280@qq.com>
2019-12-27 13:36:18 +0800

    first commit

---
+++ src/test/java/com/example/springboot_ehcache/SpringbootEhcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_ehcache;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEhcacheApplicationTests {
+
+	
+	void contextLoads() {
+	}
+
+}
